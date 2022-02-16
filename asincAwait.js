276eda74ecc300932e5cd80987763e109c997824
Anthony <sistema18@gmail.com>
2022-02-16 01:51:45 -0400

    primer comisionado

---
+++ asincAwait.js
@@ -0,0 +1,23 @@
+const datos = [
+  {
+    id: 1,
+    title: "Iron Man",
+    year: 2008,
+  },
+  {
+    id: 2,
+    title: "Spiderman: Homecoming",
+    year: 2017,
+  },
+  {
+    id: 3,
+    title: "Avenger: Endgame",
+    year: 2019,
+  },
+];
+
+const getDatos = () => {
+  return datos;
+};
+
+console.log(getDatos());
